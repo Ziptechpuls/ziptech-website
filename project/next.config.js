0574ffb6980b020f145471bd89f2c1d956dc2984
Ziptechpuls <ziptech.plus@gmail.com>
2025-04-11 10:18:07 +0900

    Add files via upload

---
+++ project/next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      }
+    ],
+    unoptimized: true
+  }
+};
+
+module.exports = nextConfig;
